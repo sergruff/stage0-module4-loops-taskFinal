49abac2477f19fc750c1882592b6effe4d8caa4a
sergruff <sergruff@gmail.com>
2023-02-23 16:08:13 +0100

    s0m4.2taskFinal

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = 1; j <= cathetusLength+i-1; j++) {
+                int print = Math.abs(cathetusLength-j)+1;
+                if(i-print>=0){
+                    System.out.print(print);
+                } else System.out.print(" ");
+            }
+            System.out.println("");
+        }
     }
 
     public static void main(String[] args) {
